d83be93a74a2924a049d6783527ceba78e5a7bac
Rob Campbell <git@raacampbell.com>
2016-06-11 11:45:07 +0300

    correct x axes on first two demo figures

--- demo_sigstar.m
+++ demo_sigstar.m
@@ -16,16 +16,31 @@ clf
 
 
 
-
 subplot(rows,cols,1)
-H=bar([1.8,2,1.2,1.9,1,2,3]);
+barData=[2,1.2,1.9,1,2,3];
+H=bar(barData);
 set(H,'FaceColor',[0.5,1,0.5])
-groups={[4,7],[2,3],[6,5]};
+groups={[3,6],[1,2],[5,4]};
+
+%overlay random data
+hold on
+for ii=1:length(barData)
+	r=randn(1,10)*0.1+barData(ii);
+	p(ii)=plot(zeros(size(r))+ii, r ,'b.');
+end
 H=sigstar(groups,[0.001,0.05,0.04]);
-title('evil bar plot that hides data')
+%now jitter the points
+for ii=1:length(p)
+	x=get(p(ii),'XData');
+	set(p(ii),'XData',x+randn(size(x))*0.05);
+end
+xlim([0.5,6.5])
+
+
 
 subplot(rows,cols,2)
-H=bar([10,7,6,5]);
+barData=[10,7,6,5];
+H=bar(barData);
 set(gca,'XTickLabel',{'X','a','b','c'})
 set(H,'FaceColor',[0.5,1,0.5])
 groups={{'X','c'},...
@@ -33,14 +48,31 @@ groups={{'X','c'},...
 		{'X','a'}};
 
 
+%overlay random data
+hold on
+for ii=1:length(barData)
+	r=randn(1,10)*0.4+barData(ii);
+	p(ii)=plot(zeros(size(r))+ii, r ,'k.');
+end
+
+%H=sigstar(groups,[0.001,0.05,0.04]);
+
+%now jitter the points
+for ii=1:length(p)
+	x=get(p(ii),'XData');
+	set(p(ii),'XData',x+randn(size(x))*0.1);
+end
+
+
 H=sigstar(groups,[0.0001,0.005,0.04]);
 set(H,'Color','b')
+
 %Extend the last arms down:
 Y=get(H(1,1),'YData'); Y(4)=7.5; set(H(1,1),'YData',Y);
 Y=get(H(2,1),'YData'); Y(4)=6.5; set(H(2,1),'YData',Y);
 Y=get(H(3,1),'YData'); Y(4)=5.5; set(H(3,1),'YData',Y);
 ylim([0,13])
-title('evil bar plot that hides data')
+xlim([0.5,4.5])
 
 
 
@@ -51,7 +83,6 @@ d(:,2)=d(:,2)+2;
 boxplot(d)
 ylim([-3,6])
 H=sigstar({[1,2],[2,3]},[]);
-title('box plots are better than bar plots')
 
 
 subplot(rows,cols,4)
@@ -80,9 +111,9 @@ if exist('notBoxPlot')
 	subplot(rows,cols,6)
 	R=randn(15,3);
 	R(:,2)=	R(:,2)+2;
-	notBoxPlot(R)
+	h=notBoxPlot(R);
+	set([h.data],'MarkerSize',3)
 	sigstar({[1,2],[3,2]},[0.05,0.05]);	
-	title('notBoxPlots are even better')
 	box on
 end
 
