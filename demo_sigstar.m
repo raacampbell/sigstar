826dabaae1df2cffccb1f734b802d31b3f291621
Rob Campbell <git@raacampbell.com>
2016-06-11 11:13:57 +0300

    highlight crappyness of bar plots

--- demo_sigstar.m
+++ demo_sigstar.m
@@ -14,13 +14,15 @@ cols=3;
 
 clf
 
-subplot(rows,cols,1)
 
+
+
+subplot(rows,cols,1)
 H=bar([1.8,2,1.2,1.9,1,2,3]);
 set(H,'FaceColor',[0.5,1,0.5])
 groups={[4,7],[2,3],[6,5]};
 H=sigstar(groups,[0.001,0.05,0.04]);
-
+title('evil bar plot that hides data')
 
 subplot(rows,cols,2)
 H=bar([10,7,6,5]);
@@ -37,8 +39,8 @@ set(H,'Color','b')
 Y=get(H(1,1),'YData'); Y(4)=7.5; set(H(1,1),'YData',Y);
 Y=get(H(2,1),'YData'); Y(4)=6.5; set(H(2,1),'YData',Y);
 Y=get(H(3,1),'YData'); Y(4)=5.5; set(H(3,1),'YData',Y);
-
 ylim([0,13])
+title('evil bar plot that hides data')
 
 
 
@@ -49,7 +51,7 @@ d(:,2)=d(:,2)+2;
 boxplot(d)
 ylim([-3,6])
 H=sigstar({[1,2],[2,3]},[]);
-
+title('box plots are better than bar plots')
 
 
 subplot(rows,cols,4)
@@ -60,13 +62,13 @@ y(5)=y(5)-5;
 y(6)=y(6)+5;
 
 plot(x,y,'o-r','MarkerFaceColor',[1,0.5,0.5])
-
 sigstar({[5,6]},[0.05]);
 xlim([1,12])
 
 
-%Grouped bar chart
+
 subplot(rows,cols,5)
+%Grouped bar chart
 y = [2 2 3; 2 5 6; 2 8 9; 6 13 15];
 bar(y)
 sigstar({[1,4],[2,4],[3,4]},[0.05,0.05,0.05]);
@@ -79,8 +81,9 @@ if exist('notBoxPlot')
 	R=randn(15,3);
 	R(:,2)=	R(:,2)+2;
 	notBoxPlot(R)
-	sigstar({[1,2],[3,2]},[0.05,0.05]);
-
+	sigstar({[1,2],[3,2]},[0.05,0.05]);	
+	title('notBoxPlots are even better')
+	box on
 end
 
 
